f843f6b456c7a37579f65450a41bbe276614fdd6
jc <jiowchern@gmail.com>
2024-09-09 02:26:28 +0800

    update ip addr

--- GameProject1-Backend.git/run-test.ps1
+++ GameProject1-Backend.git/run-test.ps1
@@ -5,7 +5,7 @@ dotnet publish .\..\Regulus.Project.GameProject1.Protocol\ -o ./
 
 copy .\..\Assets\server\*.* 
 ## dotnet tool install --tool-path .\tool Regulus.Application.Server 
-dotnet .\Regulus.Application.Server.dll launchprotocol 47536 Regulus.Project.GameProject1.Protocol.dll Regulus.Project.GameProject1.Play.dll Regulus.Project.GameProject1.Play.Server
+dotnet .\Regulus.Application.Server.dll launchprotocol 53001 Regulus.Project.GameProject1.Protocol.dll Regulus.Project.GameProject1.Play.dll Regulus.Project.GameProject1.Play.Server
 ## dotnet tool uninstall --tool-path .\tool Regulus.Application.Server 
 cd ..
 ## rd bin -Recurse
--- GameProject1-Backend.git/run.ps1
+++ GameProject1-Backend.git/run.ps1
@@ -3,7 +3,7 @@ cd bin
 dotnet publish .\..\Regulus.Project.GameProject1.Play\ -o ./
 copy .\..\Assets\server\*.* 
 dotnet tool install --tool-path .\tool Regulus.Application.Server 
-.\tool\Regulus.Application.Server.exe launchprotocol 47536 Regulus.Project.GameProject1.Protocol.dll Regulus.Project.GameProject1.Play.dll Regulus.Project.GameProject1.Play.Server
+.\tool\Regulus.Application.Server.exe launchprotocol 53001 Regulus.Project.GameProject1.Protocol.dll Regulus.Project.GameProject1.Play.dll Regulus.Project.GameProject1.Play.Server
 dotnet tool uninstall --tool-path .\tool Regulus.Application.Server 
 cd ..
 rd bin -Recurse
